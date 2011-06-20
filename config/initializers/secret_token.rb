3d3e1f63a0aad3f56e404f9d1e705509dc0eefd1
David Kormushoff <david@jotlocker.com>
2011-06-20 02:32:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThreeSeventySixMain::Application.config.secret_token = 'ce7180d46425cc166b7015def954ea8d1de7a10d5eab044b24d1fab4b316d0285a07fa43e83c6f28268946aaf213f01da07f430f0d44bb1fb3edfb411ae1080d'
